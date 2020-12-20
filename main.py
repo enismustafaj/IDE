2ed17666b56461fe77c07d8d19ebeaeb11110c75
enismustafaj <40141091+enismustafaj@users.noreply.github.com>
2020-12-20 22:58:43 +0100

    :sparkles: keyword color added

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 import tkinter as tk
-from tkinter.constants import BOTH, BOTTOM, E, LEFT, RIGHT, TOP, W, X, Y
+from tkinter.constants import BOTH, BOTTOM, E, END, LEFT, RIGHT, TOP, W, X, Y
 import utility
 
-isSaved = False
+keyWords = ['word', 'key', 'hello', 'somthing']
 
+
+currStr = ''
+def keyPressed(event):
+    global currStr
+    global keyWords
+    global editor
+    idx = '1.0'
+    editor.tag_remove('key', '1.0', END) 
+    text = editor.get('1.0', END).replace('\n',' ')
+    words = text.split(' ')
+    
+    words.remove('')
+    print(words)
+    for i in range(len(words) - 1):
+        print(words[i])
+        if words[i] in keyWords:
+            print('here')
+            idx = '1.0'
+            while True:
+                idx = editor.search(words[i], idx, nocase=1, stopindex=END)
+                if not idx: break
+                
+                
+                endidx = '%s+%dc' % (idx, len(words[i]))  
+                
+                editor.tag_add('key', idx, endidx)  
+                idx = endidx 
+            
+            editor.tag_config('key', foreground='red')
+
+
+        
 
 window = tk.Tk()
+window.geometry('800x500')
+window.bind('<Key>', keyPressed)
 
 scrollBar = tk.Scrollbar()
 scrollBar.place(x = 890, y = 35)
@@ -15,7 +49,7 @@ editor = tk.Text( height=49, width=110, yscrollcommand=scrollBar.set)
 editor.place(x = 5, y = 35)
 scrollBar.config(command = editor.yview)
 
-runButton = tk.Button( text='Run', width=10, command=lambda : utility.saveFile(editor) )
+runButton = tk.Button( text='Run', width=10, command=lambda : utility.runScript(status,editor) )
 runButton.place(x=5, y=2)
 
 status = tk.Label(text='Untitled.txt')
--- utility.py
+++ utility.py
@@ -2,18 +2,21 @@ from tkinter import filedialog
 from tkinter.constants import END
 
 def browseFiles():
-    selectFile = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
-    return selectFile
+    selectFile = filedialog.asksaveasfile(mode='a', defaultextension=".txt")
+    return selectFile.name
 
 def saveFile(contentFile):
     selectedFile = browseFiles()
-    selectedFile.write(str(contentFile.get('1.0', END)))
-    selectedFile.close()
+    with open(str(selectedFile), 'w') as f :
+        f.write(str(contentFile.get('1.0', END)))
+    return selectedFile
 
-def runScript(isSaved, status, contentFile):
-    if isSaved:
+def runScript(status, contentFile):
+    if status.cget('text') != 'Untitled.txt':
         pass
     else:
-        saveFile(contentFile)
+        file = saveFile(contentFile)
+        status.configure(text = str(file) )
         isSaved = True
+
 
