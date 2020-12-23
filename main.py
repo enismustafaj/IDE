d00140fa35c3d67c880bca042182f3a2c39db577
enismustafaj <enismustafaj@gmail.com>
2020-12-23 17:41:20 +0100

    :sparkles: got to error location

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ import tkinter as tk
 from tkinter.constants import BOTH, BOTTOM, E, END, LEFT, RIGHT, TOP, W, X, Y
 import utility
 
+# keywords that are recognized
 keyWords = ['var', 'for', 'if', 'else', 'when', 'as', 'break', 'continue', 'class', 'true', 'false', 'null', 'return ', 'val']
 
 
-currStr = ''
+# function that is trigerred when a key is pressed 
 def keyPressed(event):
     global currStr
     global keyWords
     global editor
     idx = '1.0'
-    editor.tag_remove('key', '1.0', END) 
+    editor.tag_remove('key', '1.0', END)
+    
+    # get all the words in the editor 
     text = editor.get('1.0', END).replace('\n',' ')
     words = text.split(' ')
-    
     words.remove('')
     
     for i in range(len(words) - 1):
-        
+        # if a word is a keyword add a tag to it and make it blue
         if words[i] in keyWords:
             
             idx = '1.0'
@@ -33,16 +35,38 @@ def keyPressed(event):
                 idx = endidx 
             
             editor.tag_config('key', foreground='blue')
+        
+        # if a word is a function add a tag to it and make it green
+        if '(' in words[i]:
+            func = list(words[i].split('('))[0]
+            idx = '1.0'
+            while True:
+                idx = editor.search(func, idx, nocase=1, stopindex=END)
+                if not idx: break
+                
+                
+                endidx = '%s+%dc' % (idx, len(func))  
+                
+                editor.tag_add('function', idx, endidx)  
+                idx = endidx 
+            
+            editor.tag_config('function', foreground='green')
+            
 
 
+# initialize the window
 window = tk.Tk()
 window.geometry('800x500')
+photo = tk.PhotoImage(file = './icon.png')
+window.title("KIDE")
+window.iconphoto(False, photo)
 window.bind('<Key>', keyPressed)
 
 scrollBar = tk.Scrollbar()
 scrollBar.place(x = 890, y = 35)
 scrollBar.pack(side = RIGHT, fill= Y)
 
+# add the editor pane
 editor = tk.Text( height=49, width=110, yscrollcommand=scrollBar.set)
 editor.place(x = 5, y = 35)
 scrollBar.config(command = editor.yview)
@@ -53,14 +77,15 @@ runButton.place(x=5, y=2)
 status = tk.Label(text='Untitled.txt')
 status.place(x=130, y=6)
 
+# add the output pane
 output = tk.Text(height= 49, width= 50)
 output.configure(state='disabled')
 output.place(x = 960, y = 35)
 
-runStatus = tk.Label(text = " ")
-runStatus.place(x=500, y=6)
+runStatus = tk.Label(text = "Running", fg = "gray")
+runStatus.place(x=600, y=6)
 
 returnCode = tk.Label(text = " ")
-returnCode.place(x=800, y=6)
+returnCode.place(x=900, y=6)
 
 window.mainloop()
--- utility.py
+++ utility.py
@@ -1,8 +1,11 @@
 from tkinter import filedialog
 from tkinter.constants import END
+import tkinter as tk
 import subprocess
 import re
+import time
 
+# select the file to save
 def browseFiles():
     selectFile = filedialog.asksaveasfile(mode='a', defaultextension=".kts")
     if selectFile != None:
@@ -10,19 +13,23 @@ def browseFiles():
     else:
    	    return None
 
-
+# save the file
 def saveFile(contentFile):
     selectedFile = browseFiles()
     with open(str(selectedFile), 'w') as f :
         f.write(str(contentFile.get('1.0', END)))
     return selectedFile
 
+
+# function which is trigerred when the script is running
 def runScript(status, contentFile, console, runButton, returnCode):
     
-    
+    runButton.config(fg = "green")
+	
+	# check if the file is saved
+	# if not save first then run
     if status.cget('text') != 'Untitled.txt':
-    	
-        runButton.config(text="Running")
+          
         with open(str(status.cget('text')), 'w') as f:
         	f.write(str(contentFile.get('1.0', END)))
         output = subprocess.run(["kotlinc", "-script", str(status.cget('text'))],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -30,43 +37,46 @@ def runScript(status, contentFile, console, runButton, returnCode):
         result = output.stdout.decode()
        	
     else:
-        runButton.config(text="Running")
+    
         file = saveFile(contentFile)
-        print(file)
         output = subprocess.run(["kotlinc", "-script", file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
         result = output.stdout.decode()
         status.configure(text = str(file) )
-        
-    print(findError(str(result)))
-    errors = findError(str(result))
+    
+    # get all the error locations in the code
+    errors = list(findError(str(result)))
+    
     returnCode.config(text = "Return code: " + str(output.returncode))
+    
+    # write the output ot output pane
     console.configure(state='normal')
+    console.delete('1.0', END)
     console.insert('end', result)
+    count = 0
+    
+    # add a tag to each error and funtion to redirect to the location
     for i in errors:
         idx = '1.0'
-        while True:
-            idx = console.search(i, idx, nocase=1, stopindex=END)
-            if not idx: break
-             
-                
-            endidx = '%s+%dc' % (idx, len(i))  
-               
-            console.tag_add(i, idx, endidx)  
-            idx = endidx 
-          
-        console.tag_config(i, foreground='red', underline = True)
-        console.tag_bind(i, "<Button-1>", lambda e: goTo(i, contentFile))
+        idx = console.search(i, idx, nocase=1, stopindex=END)
+        endidx = '%s+%dc' % (idx, len(i))        
+        console.tag_add("err " + str(count), idx, endidx)   
+        console.tag_config("err " + str(count), foreground='red', underline = True)
+        console.tag_bind("err " + str(count), "<Button-1>", lambda l: goTo(i, contentFile))
+        count += 1
         
     console.configure(state='disabled')
-    runButton.config(text = "Run")
     
+    runButton.configure(fg = "gray")
+
+# function to get all the errors    
 def findError(errorMsg):
     err = re.findall("[a-zA-Z]*\.[a-z]*:[1-9]*:[1-9]*", errorMsg)
-    return err
-    
+    return set(err)
+
+#function to go to the location of the error    
 def goTo(err, console):
     rowcol = err.split(':')
-    print(rowcol)
-    console.mark_set("insert", "%d.%d" % (3 , 5 ))
+    idx = console.search(err, '1.0', nocase=1, stopindex=END)
+    console.mark_set("insert", idx)
     
